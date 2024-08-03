f4a99a1c0259323b0ee4b808a7a10673d588b6dc
Loc Nguyen <nguyenducloc404@gmail.com>
2024-08-03 18:14:20 +0700

    style(home-page): smooth scroll and wrap overflow element

--- src/components/Card/PostCard/index.tsx
+++ src/components/Card/PostCard/index.tsx
@@ -78,10 +78,20 @@ export default function PostCard() {
         </DropdownMenu>
       </div>
       <CardContent>
-        <p className="mt-4 font-bold text-2xl">His mother had always taught him</p>
-        <div className="flex gap-2 mt-1">
+        <p className="mt-4 font-bold text-2xl break-words">
+          His mother had always taught him to fdshaf tothsd sat hestlaste htiasekt eashtkesatkls
+        </p>
+        <div className="flex gap-2 mt-1 flex-wrap">
           <Badge variant="outline">#history</Badge>
           <Badge variant="outline">#american</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
+          <Badge variant="outline">#crime</Badge>
           <Badge variant="outline">#crime</Badge>
         </div>
       </CardContent>
