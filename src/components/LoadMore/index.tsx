71a1ab4e43f2691f09056768b3ff82cfdffb06e3
Loc Nguyen <nguyenducloc404@gmail.com>
2024-08-06 19:42:29 +0700

    feat: infinite scroll using server actions

--- src/components/LoadMore/index.tsx
+++ src/components/LoadMore/index.tsx
@@ -8,32 +8,22 @@ import { useInView } from 'react-intersection-observer';
 import Spinner from '../common/Spinner';
 import PostCard from '../Card/PostCard';
 
-let page = 2;
 export default function LoadMore({ fetchUrl }: { fetchUrl: string }) {
   const { ref, inView } = useInView();
   const [posts, setPosts] = useState<Post[]>([]);
-  // const [page, setPage] = useState<number | null>(2);
+  const [page, setPage] = useState<number | null>(2);
 
-  if (inView) {
-    if (page === null) return;
-    getPosts(fetchUrl, { page: page ?? 0 }).then(res => {
-      setPosts([...posts, ...res.posts]);
-      // setPage(res.nextPage);
-    });
-    // .catch(() => setPage(null));
-  }
-
-  // useEffect(() => {
-  //   if (inView) {
-  //     if (page === null) return;
-  //     getPosts(fetchUrl, { page: page ?? 0 })
-  //       .then(res => {
-  //         setPosts([...posts, ...res.posts]);
-  //         setPage(res.nextPage);
-  //       })
-  //       .catch(() => setPage(null));
-  //   }
-  // }, [inView, posts, fetchUrl, page]);
+  useEffect(() => {
+    if (inView) {
+      if (page === null) return;
+      getPosts(fetchUrl, { page: page ?? 0 })
+        .then(res => {
+          setPosts([...posts, ...res.posts]);
+          setPage(res.nextPage);
+        })
+        .catch(() => setPage(null));
+    }
+  }, [inView, posts, fetchUrl, page]);
 
   return (
     <>
