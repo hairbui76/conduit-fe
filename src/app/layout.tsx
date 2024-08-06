91cbb196afef4e1e44c585645f63d09a8ebfbc0a
Loc Nguyen <nguyenducloc404@gmail.com>
2024-08-06 15:40:51 +0700

    perf: add metadata for home and view post page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { cookies } from 'next/headers';
 
 import '../styles/globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from 'react-hot-toast';
 import ProgressBarProvider from '@/contexts/progressbar';
 import { Sidebar } from '@/components/Sidebar';
-import { getCurrentUser } from '@/actions/user';
 import Section from '@/containers/root-page/section';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,7 +16,12 @@ export const metadata: Metadata = {
     default: 'Conduit'
   },
   description:
-    "Conduit is a dynamic social media platform designed to bring people together through meaningful conversations and shared experiences. Whether you're looking to connect with like-minded individuals, share your stories, or discover new perspectives, Conduit is your go-to place. Our intuitive interface and diverse community make it easy to engage, express, and inspire. Join Conduit today and be part of a network where your voice matters and connections flourish."
+    "Conduit is a dynamic social media platform designed to bring people together through meaningful conversations and shared experiences. Whether you're looking to connect with like-minded individuals, share your stories, or discover new perspectives, Conduit is your go-to place. Our intuitive interface and diverse community make it easy to engage, express, and inspire. Join Conduit today and be part of a network where your voice matters and connections flourish.",
+  openGraph: {
+    title: 'Conduit',
+    description: 'Conduit is dynamic social media platform',
+    siteName: 'Conduit'
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/app/post/[postSlug]/page.tsx
+++ src/app/post/[postSlug]/page.tsx
@@ -1,6 +1,30 @@
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 import { getSinglePost } from '@/actions/post';
 import SinglePostSection from '@/containers/single-post-page/single-post-section';
-import { notFound } from 'next/navigation';
+
+export const generateMetadata = async ({ params }: { params: { postSlug: string } }) => {
+  const data = await getSinglePost(params.postSlug);
+
+  if (!data)
+    return {
+      title: 'Post not found',
+      description: 'This post can not be found'
+    };
+
+  return {
+    title: data.post.title,
+    description: data.post.description,
+    openGraph: {
+      title: data.post.title,
+      description: data.post.description,
+      type: 'article',
+      publishedTime: data.post.createdAt,
+      authors: [data.post.author]
+    }
+  };
+};
 
 export default async function Page({ params }: { params: { postSlug: string } }) {
   const data = await getSinglePost(params.postSlug);
