f54649f2c3d2630d61ee2788dd7cae9e6a9f0846
Loc Nguyen <nguyenducloc404@gmail.com>
2024-08-07 17:34:23 +0700

    feat: filter tags

--- src/components/Card/TagsCard/index.tsx
+++ src/components/Card/TagsCard/index.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import Tag from '@/components/Tag';
 import {
   Carousel,
@@ -8,17 +12,31 @@ import {
 } from '@/components/Carousel';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/common/Card';
 import { chunk } from 'lodash';
+import { Input } from '@/components/common/Input';
+import { Search } from 'lucide-react';
 
 export default function TagsCard({ tags, className }: { tags: string[]; className: string }) {
+  const [searchValue, setSearchValue] = useState('');
+
+  let filteredTags = tags.filter(tag => tag.startsWith(searchValue));
+
   return (
     <Card className={className}>
       <CardHeader>
         <CardTitle>Tags</CardTitle>
       </CardHeader>
       <CardContent>
+        <div className="relative mb-4">
+          <Search className="absolute left-2 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+          <Input
+            className="h-8 rounded-sm pl-8"
+            placeholder="Search tag"
+            onChange={e => setSearchValue(e.target.value)}
+          />
+        </div>
         <Carousel>
           <CarouselContent>
-            {chunk(tags, 20).map((each, index) => (
+            {chunk(filteredTags, 20).map((each, index) => (
               <CarouselItem key={`tags-${index}`} className="flex gap-2 flex-wrap">
                 {each.map(tag => (tag.length > 0 ? <Tag key={tag} tag={tag} /> : null))}
               </CarouselItem>
